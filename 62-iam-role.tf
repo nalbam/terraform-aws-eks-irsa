51ad3546070969fbfe19cf7c9460dad76fae4208
nalbam <me@nalbam.com>
2020-08-12 15:37:21 +0900

    description

--- 62-iam-role.tf
+++ 62-iam-role.tf
@@ -1,8 +1,9 @@
 # iam role
 
 resource "aws_iam_role" "this" {
-  name = var.name
-  path = "/"
+  name        = var.name
+  path        = "/"
+  description = "irsa iam_role for ${var.name}"
 
   # Maximum CLI/API session duration in seconds between 3600 and 43200"
   max_session_duration = 3600
@@ -10,7 +11,7 @@ resource "aws_iam_role" "this" {
   force_detach_policies = false
   permissions_boundary  = ""
 
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.this.json
 
   tags = merge(
     {
@@ -20,7 +21,7 @@ resource "aws_iam_role" "this" {
   )
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
+data "aws_iam_policy_document" "this" {
   statement {
     effect = "Allow"
 
--- 63-iam-policy.tf
+++ 63-iam-policy.tf
@@ -1,13 +1,13 @@
 # iam policy
 
+resource "aws_iam_policy" "this" {
+  name        = var.name
+  path        = "/"
+  description = "irsa iam_policy for ${var.name}"
+  policy      = var.policy_document
+}
+
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
-}
-
-resource "aws_iam_policy" "this" {
-  name        = var.name
-  description = "Autoscaling policy for ${var.name}"
-  policy      = var.policy_document
-  path        = "/"
 }

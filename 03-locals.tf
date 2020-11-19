9964b19c2863e261105b5d1cf81a5ba6102601a2
nalbam <me@nalbam.com>
2020-11-19 15:34:08 +0900

    provider_urns

--- 03-locals.tf
+++ 03-locals.tf
@@ -5,8 +5,8 @@ locals {
 }
 
 locals {
-  provider_url = data.aws_eks_cluster.cluster.0.identity.0.oidc.0.issuer
-  provider_urn = replace(local.provider_url, "https://", "")
+  # provider_url = data.aws_eks_cluster.cluster.0.identity.0.oidc.0.issuer
+  # provider_urn = replace(local.provider_url, "https://", "")
 
   provider_urns = [
     for item in data.aws_eks_cluster.cluster.*.identity.0.oidc.0.issuer :

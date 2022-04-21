09a9cdbb8ea3641b888e37fb0f1bdade22e9d021
nalbam <me@nalbam.com>
2022-04-21 14:02:40 +0900

    cluster_names default

--- 03-locals.tf
+++ 03-locals.tf
@@ -6,7 +6,7 @@ locals {
 
 locals {
   cluster_group = var.cluster_group != null ? var.cluster_group : var.cluster_name
-  cluster_names = var.cluster_names != null ? var.cluster_names : [local.cluster_group]
+  cluster_names = var.cluster_names != null ? var.cluster_names : []
 
   name = var.irsa_name != null ? var.irsa_name : format("irsa--%s--%s", local.cluster_group, var.service_name)
 
